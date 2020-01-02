e279051530115cc5e3d2cb9008bd4139bf59b2a3
Kendall Jackson <kendallkj00@gmail.com>
2020-01-02 11:01:02 -0500

    Lint

--- src/components/SecondSection.vue
+++ src/components/SecondSection.vue
@@ -3,7 +3,7 @@
     <!-- Two columns -->
     <div class="flex mb-4 text-center">
       <div class="w-1/2 bg-gray-400 h-12">
-      text content paragraph
+        text content paragraph
       </div>
       <div class="w-1/2 bg-gray-500 h-12">
         video rounded popup
