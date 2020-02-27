ab3f7efd465f9b3a5f57354c133988921fa1e07d
Kendall Jackson <kendallkj00@gmail.com>
2020-02-26 22:39:40 -0500

    Trying TUi

---
+++ src/components/TailSub.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="bg-white">
+    <div class="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 md:py-16 lg:px-8 lg:py-20">
+      <h2 class="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
+        Ready to get started?
+        <br>
+        <span class="text-blue-600">Get in touch or create an account.</span>
+      </h2>
+      <div class="mt-8 flex">
+        <div class="inline-flex rounded-md shadow">
+          <a href="#" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
+            Get started
+          </a>
+        </div>
+        <div class="ml-3 inline-flex">
+          <a href="#" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-blue-700 bg-blue-100 hover:text-blue-600 hover:bg-blue-50 focus:outline-none focus:shadow-outline focus:border-blue-300 transition duration-150 ease-in-out">
+            Learn more
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TailSub'
+}
+</script>
---
+++ src/pages/index2.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="pageroot bg-gray-100 antialiased font-sans">
+    <TailHero />
+    <TailFeat />
+    <TailTest />
+    <TailStats />
+    <TailSub />
+    <TailFoot />
+  </div>
+</template>
+
+<script>
+import TailHero from '~/components/TailHero'
+import TailFeat from '~/components/TailFeat'
+import TailTest from '~/components/TailTest'
+import TailStats from '~/components/TailStats'
+import TailSub from '~/components/TailSub'
+import TailFoot from '~/components/TailFoot'
+export default {
+  name: 'index2',
+  layout: 'checkout',
+  components: {
+    TailHero,
+    TailFeat,
+    TailTest,
+    TailStats,
+    TailSub,
+    TailFoot
+  }
+}
+</script>
