6541b5aa8509a4f26cee211bad30b6bc3ea50031
Kendall Jackson <kendallkj00@gmail.com>
2020-04-23 05:19:19 -0400

    data-not-lazy

--- src/components/TailHero.vue
+++ src/components/TailHero.vue
@@ -96,10 +96,10 @@
       </div>
     </div>
     <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-      <picture>
+      <picture data-not-lazy>
         <source srcset="~assets/img/janitor.webp" type="image/webp">
         <source srcset="~assets/img/janitor.jpg" type="image/jpeg">
-        <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="~assets/img/janitor.jpg" alt="Janitorial equipment">
+        <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="~assets/img/janitor.jpg" alt="Janitorial equipment" data-not-lazy>
       </picture>
       <!-- <picture data-not-lazy>
         <source srcset="~assets/img/janitor.webp" type="image/webp">
