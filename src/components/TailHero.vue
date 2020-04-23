a2087dcb8388cf675e4f79034b744a2c0316ac9e
Kendall Jackson <kendallkj00@gmail.com>
2020-04-23 01:00:22 -0400

    Accessibility fixes

--- src/components/TailHero.vue
+++ src/components/TailHero.vue
@@ -6,9 +6,9 @@
           <nav class="relative flex items-center justify-between sm:h-10 lg:justify-start">
             <div class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
               <div class="flex items-center justify-between w-full md:w-auto">
-                <a href="#">
+                <nuxt-link to="/">
                   <img class="h-8 w-auto sm:h-10 sm:w-auto" src="~/static/logo.png" alt="">
-                </a>
+                </nuxt-link>
                 <div class="-mr-2 flex items-center md:hidden">
                   <button type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out" @click="open = true">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
@@ -44,7 +44,7 @@
                   <img class="h-8 w-auto" src="~/static/logo.png" alt="">
                 </div>
                 <div class="-mr-2">
-                  <button type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out" @click="open = false">
+                  <button aria-label="Open menu" type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out" @click="open = false">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
