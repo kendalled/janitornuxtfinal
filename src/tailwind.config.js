f77586516f5d85df47b04e380390da320b5b7de5
Kendall Jackson <kendallkj00@gmail.com>
2020-05-05 14:45:11 -0400

    Update dependencies

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -2,6 +2,17 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js'
+    ],
+    defaultExtractor: (content) => content.match(/[\w-/.:]+(?<!:)/g) || []
+  },
   theme: {
     extend: {
       fontFamily: {
